1d72344730f08451c46d24a7796241e9d3f79dc7
hongseongun <hongseongun@github.com>
2021-04-13 12:44:32 +0900

    Add code to create a .whl file

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # netspresso-client
 
+## make .whl file
+```python
+python setup.py bdist_wheel
+```
+## install 
+```python
+pip install netspresso_cli-0.1.0-py3-none-any.whl
+```
+
 ## Run
 ```python
 python netspresso_cli/main.py --config config_files/example1.yaml
--- setup.py
+++ setup.py
@@ -16,6 +16,30 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     include_package_data=True,
-    packages=setuptools.find_packages(where="netspresso_cli"),
+    packages=setuptools.find_packages(),
     python_requires=">=3.7",
+    install_requires= [
+        "boto3==1.17.38",
+        "botocore==1.20.38",
+        "certifi==2020.12.5",
+        "chardet==4.0.0",
+        "idna==2.10",
+        "jmespath==0.10.0",
+        "numpy==1.20.1",
+        "pandas==1.2.3",
+        "python-dateutil==2.8.1",
+        "pytz==2021.1",
+        "PyYAML==5.4.1",
+        "requests==2.25.1",
+        "s3transfer==0.3.6",
+        "six==1.15.0",
+        "tqdm==4.59.0",
+        "urllib3==1.26.4"
+    ],
+
+    entry_points={
+        'console_scripts':[
+            'netspresso-cli=netspresso_cli.main:main',
+        ],
+    },
 )
