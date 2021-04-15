3860da25bd86f7898a8a986ef7c9270907f64798
dwlim_nota <dwlim@nota.ai>
2021-04-15 22:23:22 +0900

    set public domain

--- netspresso_cli/settings.py
+++ netspresso_cli/settings.py
@@ -7,7 +7,7 @@ os.environ["AWS_SECRET_KEY"] = "uPYB+c/07JCvw3p700b1mYT//XuM6g1Nn2KmYx9t"
 class API_SERVER:
     # HOST = "localhost"
     # PORT = 22080
-    HOST = "aca5518346ebb46a0a97ed3c4276e02e-2020964609.us-east-2.elb.amazonaws.com"
+    HOST = "www.netspresso.co.kr"
     PORT = 8000
 
 RESULT_DIR = "result"
