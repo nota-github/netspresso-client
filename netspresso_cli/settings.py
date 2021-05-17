2e06ac73c02488afae366aad3e522c4f608f6970
dwlim_nota <dwlim@nota.ai>
2021-05-17 19:41:37 +0900

    generalize

--- netspresso_cli/settings.py
+++ netspresso_cli/settings.py
@@ -3,7 +3,7 @@ import os
 class API_SERVER:
     # HOST = "localhost"
     # PORT = 22080
-    HOST = "www.netspresso.co.kr"
+    HOST = "test2.netspresso.co.kr"
     PORT = 8000
 
 RESULT_DIR = "result"
